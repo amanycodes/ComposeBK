48f9e44bf1404723740f7986e4337a80c2968cfc
amanycodes <amanycodes@gmail.com>
2025-08-06 23:51:33 +0530

    test llb
    Signed-off-by: amanycodes <amanycodes@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/moby/buildkit/client/llb"
+)
 
 func main() {
-	fmt.Print("inital commit")
+	alpine := llb.Image("docker.io/library/alpine:latest")
+
+	builder := alpine.Run(
+		llb.Shlex("echo 'Hello, BuildKit!' > /hello"),
+	)
+
+	finalState := builder.Root()
+
+	def, err := finalState.Marshal(
+		context.Background(),
+		llb.LinuxAmd64,
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error marshaling LLB: %v\n", err)
+		os.Exit(1)
+	}
+
+	llb.WriteTo(def, os.Stdout)
 }
